37fbbd35969139581f6ac415bad1c47dc571debb
jgchoti <jgchotirat@gmail.com>
2024-05-13 22:28:42 +0200

    Update constants.js

--- client/data/constants.js
+++ client/data/constants.js
@@ -4,12 +4,18 @@
 export const TABLE_HEAD_ID = {
     'Status': 'status',
     'Task': 'task',
-    'Due': 'due'
+    'Due': 'due',
+    'Remove': 'remove'
 }
+
+export const TABLE_CONTAINER_ID = 'table-container'
+
 // classes
 export const TABLE_CLASS_NAME = {
     'Status': 'status',
     'Task': 'task',
-    'Due': 'due'
+    'Due': 'due',
+    'Remove': 'remove'
 }
 // ... other things?
+export const BIN_ICON = '<i class="fa-solid fa-trash-can"></i>'
