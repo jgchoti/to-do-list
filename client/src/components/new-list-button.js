b8c32c0bef4262b31efa2d462ccb34a70a1ef190
jgchoti <jgchotirat@gmail.com>
2024-05-13 23:14:26 +0200

    Update new-list-button.js

--- client/src/components/new-list-button.js
+++ client/src/components/new-list-button.js
@@ -2,6 +2,7 @@ export const newListButton = () => {
     const container = document.getElementById('table-container')
     const buttonEl = document.createElement('button')
     buttonEl.innerHTML = '+ New'
+    buttonEl.id = 'new-list'
     container.appendChild(buttonEl)
     return buttonEl;
 }
