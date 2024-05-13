55cbe9a4a1cec8436a9f59b79dc652be762176d6
jgchoti <jgchotirat@gmail.com>
2024-05-13 23:14:35 +0200

    Update init.js

--- client/src/init.js
+++ client/src/init.js
@@ -1,7 +1,11 @@
 import { tableHeader } from './components/table-header.js'
+import { header } from './components/header.js'
 import { createTable } from './components/table.js'
 import { newListButton } from './components/new-list-button.js'
+import { newListListeners } from './listeners/new-list-listeners.js'
 
 tableHeader()
+header()
 createTable()
 newListButton()
+newListListeners()
