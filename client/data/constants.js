9c06b3e99dccaf41d8adc64beeaba72e79c69f1a
jgchoti <jgchotirat@gmail.com>
2024-05-13 16:38:28 +0200

    Update constants.js

--- client/data/constants.js
+++ client/data/constants.js
@@ -1,7 +1,15 @@
 // event types
 
 // IDs
-
+export const TABLE_HEAD_ID = {
+    'Status': 'status',
+    'Task': 'task',
+    'Due': 'due'
+}
 // classes
-
+export const TABLE_CLASS_NAME = {
+    'Status': 'status',
+    'Task': 'task',
+    'Due': 'due'
+}
 // ... other things?
