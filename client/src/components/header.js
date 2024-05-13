40f46d46aec55f42528b601063d3828078903bc6
jgchoti <jgchotirat@gmail.com>
2024-05-13 23:14:24 +0200

    Create header.js

---
+++ client/src/components/header.js
@@ -0,0 +1,8 @@
+import { TEXT_HEADER } from "../../data/constants.js"
+export const header = () => {
+    const h1El = document.createElement('h1')
+    const container = document.getElementById('table-container')
+    h1El.innerText = TEXT_HEADER
+    container.insertBefore(h1El, container.firstElementChild)
+    return h1El
+}
