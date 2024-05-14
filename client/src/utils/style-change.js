81b68eb02707c60019dd8b96871d0b9a369c138a
jgchoti <jgchotirat@gmail.com>
2024-05-14 16:03:03 +0200

    Update style-change.js

--- client/src/utils/style-change.js
+++ client/src/utils/style-change.js
@@ -1,7 +1,9 @@
 export const styleChange = (element, isChecked) => {
     if (isChecked) {
         element.classList.add('checked-label');
+        element.firstElementChild.setAttribute('contenteditable', 'false');
     } else {
         element.classList.remove('checked-label');
+        element.firstElementChild.setAttribute('contenteditable', 'true');
     }
 }
