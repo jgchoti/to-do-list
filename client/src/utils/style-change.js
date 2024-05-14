be7a62a581d5dcb89f9f90db520e6f3162817bb2
jgchoti <jgchotirat@gmail.com>
2024-05-14 21:11:40 +0200

    Update style-change.js

--- client/src/utils/style-change.js
+++ client/src/utils/style-change.js
@@ -1,4 +1,8 @@
 export const styleChange = (element, isChecked) => {
+    if (element.textContent.startsWith("DONE:")) {
+        const newText = `<strong>${element.textContent.substring(0, 6)}</strong>${element.textContent.substring(6)}`;
+        element.innerHTML = newText;
+    }
     if (isChecked) {
         element.classList.add('checked-label');
         element.firstElementChild.setAttribute('contenteditable', 'false');
