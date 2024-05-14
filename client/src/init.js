73dc254249e3b3e6a4aad0fcdcbd38984a28430c
jgchoti <jgchotirat@gmail.com>
2024-05-14 21:11:22 +0200

    Update init.js

--- client/src/init.js
+++ client/src/init.js
@@ -3,9 +3,24 @@ import { header } from './components/header.js'
 import { createTable } from './components/table.js'
 import { newListButton } from './components/new-list-button.js'
 import { newListListeners } from './listeners/new-list-listeners.js'
+import { saveList } from './handlers/save-list.js'
+import { state } from '../data/state.js'
+import { loadList } from './handlers/load-list.js'
+import { saveButton } from './components/save-button.js'
+import { saveButtonListeners } from './listeners/save-listeners.js'
+import { resetButton } from './components/reset-button.js'
+import { resetListeners } from './listeners/reset-listeners.js'
+import { createButtons } from './components/buttons.js'
+import { buttonsListeners } from './listeners/buttons-listeners.js'
 
 tableHeader()
 header()
-createTable()
-newListButton()
-newListListeners()
+loadList()
+createButtons()
+buttonsListeners()
+// newListButton()
+// saveButton()
+// resetButton()
+// newListListeners()
+// saveButtonListeners()
+// resetListeners()
