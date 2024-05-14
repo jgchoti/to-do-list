ada684d0643f32208f83a46aa84370905a193377
jgchoti <jgchotirat@gmail.com>
2024-05-14 21:11:06 +0200

    Update constants.js

--- client/data/constants.js
+++ client/data/constants.js
@@ -1,3 +1,8 @@
+import { saveList } from '../../client/src/handlers/save-list.js';
+import { newListHandlers } from '../../client/src/handlers/new-list-handlers.js';
+import { reset } from '../../client/src/handlers/reset.js';
+
+
 // event types
 export const CLICK = 'click'
 export const CHANGE = 'change'
@@ -11,6 +16,8 @@ export const TABLE_HEAD_ID = {
 
 export const TABLE_CONTAINER_ID = 'table-container'
 export const NEW_LIST_ID = 'new-list'
+export const SAVE_BTN_ID = 'save-btn'
+export const RESET_BTN_ID = 'reset-btn'
 
 // classes
 export const TABLE_CLASS_NAME = {
@@ -20,6 +27,30 @@ export const TABLE_CLASS_NAME = {
     'remove': 'remove'
 }
 export const REMOVE_BTN_CLASS = 'remove-btn'
+
+
 // ... other things?
 export const TEXT_HEADER = 'TO-DO LIST'
 export const BIN_ICON = '<i class="fa-solid fa-trash-can"></i>'
+export const SAVE_ICON = '<i class="fa-solid fa-floppy-disk"></i>'
+export const RESET_ICON = '<i class="fa-solid fa-arrow-rotate-left"></i >'
+
+
+export const NEW_TASK = {
+    'status': [''],
+    'task': ['Click to Edit'],
+    'due': [''],
+    'remove': ['']
+};
+
+export const BUTTONS = {
+    'new-list': '+ New',
+    'save-btn': '<i class="fa-solid fa-floppy-disk"></i>',
+    'reset-btn': '<i class="fa-solid fa-arrow-rotate-left"></i >'
+};
+
+export const BUTTONS_HANDLERS = {
+    'new-list': newListHandlers,
+    'save-btn': saveList,
+    'reset-btn': reset
+};
