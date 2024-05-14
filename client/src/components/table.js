3ded86ea17c6b4caf84dc2cf4be260a6294bad34
jgchoti <jgchotirat@gmail.com>
2024-05-14 21:11:48 +0200

    Update table.js

--- client/src/components/table.js
+++ client/src/components/table.js
@@ -1,50 +1,45 @@
-import { state } from "../../data/state.js"
-import { TABLE_CLASS_NAME } from "../../data/constants.js"
-import { checkbox } from "./checkbox.js"
-import { dateInput } from "./date-input.js"
-import { textInput } from "./text-input.js"
-import { createRemoveButton } from './remove-button.js'
-import { generateID } from '../utils/generate-id.js'
-// event listener for check-box to change style for text Element
+import { TABLE_CLASS_NAME } from "../../data/constants.js";
+import { checkbox } from "./checkbox.js";
+import { dateInput } from "./date-input.js";
+import { textInput } from "./text-input.js";
+import { createRemoveButton } from './remove-button.js';
+import { generateID } from '../utils/generate-id.js';
 import { checkedListeners } from "../listeners/checked-listeners.js";
+import { NEW_TASK } from "../../data/constants.js";
+
 
 export const createTable = () => {
-    const tbodyEl = document.querySelector('tbody');
-
-    const taskItems = state['task'];
-
+    const tbodyEl = document.getElementById('todo-table');
+    let taskItems = NEW_TASK['task'];
+    let object = NEW_TASK
 
     for (const item of taskItems) {
         const trEl = document.createElement('tr');
         const taskId = generateID();
-        for (const key in state) {
+        for (const key in object) {
             const tdEl = document.createElement('td');
-            trEl.appendChild(tdEl)
-            tdEl.classList.add(TABLE_CLASS_NAME[key])
+            trEl.appendChild(tdEl);
+            tdEl.classList.add(TABLE_CLASS_NAME[key]);
         }
+        tbodyEl.appendChild(trEl);
 
-        const tdStatus = trEl.firstElementChild
+        const tdStatus = trEl.firstElementChild;
+        const checkboxEl = checkbox(taskId);
+        tdStatus.appendChild(checkboxEl);
 
-        const checkboxEl = checkbox(taskId)
-        tdStatus.appendChild(checkboxEl)
-
-        const tdTask = trEl.children[1]
-        const textEl = textInput(item, taskId)
-        tdTask.appendChild(textEl)
+        const tdTask = trEl.children[1];
+        const textEl = textInput(item, taskId); // Adjusted to pass item directly
+        tdTask.appendChild(textEl);
 
         checkedListeners(checkboxEl, textEl);
 
-        const tdDue = trEl.children[2]
-        const dateInputEl = dateInput()
-        tdDue.appendChild(dateInputEl)
+        const tdDue = trEl.children[2];
+        const dateInputEl = dateInput();
+        tdDue.appendChild(dateInputEl);
 
-        const tdRemove = trEl.lastElementChild
-        const removeBtnEl = createRemoveButton(tdRemove)
-        tdRemove.appendChild(removeBtnEl)
+        const tdRemove = trEl.lastElementChild;
+        const removeBtnEl = createRemoveButton(tdRemove);
+        tdRemove.appendChild(removeBtnEl);
 
-
-        tbodyEl.appendChild(trEl);
     }
-
 };
-
