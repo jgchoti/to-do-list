faf22c6c5e6ffba2318778866707c9cbcc3d93b8
jgchoti <jgchotirat@gmail.com>
2024-05-14 16:02:58 +0200

    Update checked-handlers.js

--- client/src/handlers/checked-handlers.js
+++ client/src/handlers/checked-handlers.js
@@ -1,10 +1,16 @@
 import { styleChange } from '../utils/style-change.js'
 export const checkedHandlers = (checkboxEl, textInputEl) => {
     let isChecked = false
+    const textEl = textInputEl.firstElementChild
     if (checkboxEl.checked) {
         isChecked = true
         styleChange(textInputEl, isChecked)
+        textEl.innerHTML = `<strong>DONE:</strong> ${textEl.textContent} !`
     } else {
         styleChange(textInputEl, isChecked)
+        if (textEl.textContent.startsWith("DONE")) {
+            textEl.textContent = textEl.textContent.slice(6);
+            textEl.textContent = textEl.textContent.slice(0, -1);
+        }
     }
 }
