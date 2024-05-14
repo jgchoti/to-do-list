9b7faefa23a4d0e6d8c8477f23d7a09c88b0d4ac
jgchoti <jgchotirat@gmail.com>
2024-05-14 16:03:01 +0200

    Update checked-listeners.js

--- client/src/listeners/checked-listeners.js
+++ client/src/listeners/checked-listeners.js
@@ -2,9 +2,14 @@ import { CHANGE } from '../../data/constants.js'
 import { checkedHandlers } from '../handlers/checked-handlers.js'
 
 export const checkedListeners = (checkboxEl, textInputEl) => {
-    checkboxEl.addEventListener(CHANGE, () => checkedHandlers(checkboxEl, textInputEl))
-}
 
+    checkboxEl.addEventListener(CHANGE, (event) => {
+        if (event.target === checkboxEl) {
+            checkedHandlers(checkboxEl, textInputEl);
+        }
+        console.log(event.target)
+    })
+}
 
 
 
