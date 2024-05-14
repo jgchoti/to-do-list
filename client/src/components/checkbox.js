efdbad892d7f6abc698bb20fb0e9b075a992875a
jgchoti <jgchotirat@gmail.com>
2024-05-14 12:39:37 +0200

    Update checkbox.js

--- client/src/components/checkbox.js
+++ client/src/components/checkbox.js
@@ -1,11 +1,8 @@
-import { checkedListeners } from "../listeners/checked-listeners.js";
-
 export const checkbox = (id) => {
     const checkboxEl = document.createElement('input')
     const element = 'checkbox'
     checkboxEl.type = element
     checkboxEl.id = id
     checkboxEl.value = id
-    checkedListeners(checkboxEl)
     return checkboxEl;
 }
