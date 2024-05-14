4b371a99e3d5937cc0697aeecc0fe1e0ef0c7330
jgchoti <jgchotirat@gmail.com>
2024-05-14 12:03:56 +0200

    Update remove-button.js

--- client/src/components/remove-button.js
+++ client/src/components/remove-button.js
@@ -1,7 +1,7 @@
 import { BIN_ICON, REMOVE_BTN_CLASS } from "../../data/constants.js"
 import { removeButtonListeners } from "../listeners/remove-listeners.js"
 
-export const createRemoveButton = (element) => {
+export const createRemoveButton = () => {
     const buttonEl = document.createElement('button')
     buttonEl.innerHTML = BIN_ICON
     buttonEl.classList.add(REMOVE_BTN_CLASS)
