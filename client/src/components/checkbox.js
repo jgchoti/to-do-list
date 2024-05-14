4bb66ebdc08edc618209be32107ca782e535386c
jgchoti <jgchotirat@gmail.com>
2024-05-14 16:02:53 +0200

    Update checkbox.js

--- client/src/components/checkbox.js
+++ client/src/components/checkbox.js
@@ -2,6 +2,7 @@ export const checkbox = (id) => {
     const checkboxEl = document.createElement('input')
     const element = 'checkbox'
     checkboxEl.type = element
+    checkboxEl.classList.add(element)
     checkboxEl.id = id
     checkboxEl.value = id
     return checkboxEl;
