9efeccdbe343b723530e9a14daf3fae6912e0de5
jgchoti <jgchotirat@gmail.com>
2024-05-15 23:15:51 +0200

    Create modal-toggle.js

---
+++ client/src/handlers/modal-toggle.js
@@ -0,0 +1,13 @@
+export const modalToggle = (display) => {
+    const modal = document.getElementById('modal-body');
+    const rootDivEl = document.getElementById('root')
+    if (display) {
+        modal.style.display = 'block';
+        rootDivEl.classList.add('overlay')
+    } else {
+        modal.style.display = 'none';
+        rootDivEl.classList.remove('overlay')
+        document.getElementById('textInput').value = ''
+        document.getElementById('dateInput').value = ''
+    }
+}
