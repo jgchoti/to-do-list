42e8578917351409f2ffb1f2d7e8ee36a2f30dae
jgchoti <jgchotirat@gmail.com>
2024-05-15 20:50:10 +0200

    Update style-change.js

--- client/src/utils/style-change.js
+++ client/src/utils/style-change.js
@@ -6,9 +6,14 @@ export const styleChange = (element, isChecked) => {
         if (textEl.textContent.startsWith("DONE:")) {
             const newText = `<strong>${textEl.textContent.substring(0, 6)}</strong>${textEl.textContent.substring(6)}`;
             textEl.innerHTML = newText;
-        }
+        } else { textEl.innerHTML = `<strong>DONE:</strong> ${textEl.textContent} !` }
+
     } else {
         textEl.classList.remove('checked-label');
         textEl.setAttribute('contenteditable', 'true');
+        if (textEl.textContent.startsWith("DONE")) {
+            textEl.textContent = textEl.textContent.slice(6);
+            textEl.textContent = textEl.textContent.slice(0, -1);
+        }
     }
 }
