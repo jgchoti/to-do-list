a672e634f4953ea0caf90979d8d5fd6f3d1804ed
jgchoti <jgchotirat@gmail.com>
2024-05-15 23:15:16 +0200

    Create modal_buttons.js

---
+++ client/src/components/modal_buttons.js
@@ -0,0 +1,11 @@
+import { MODAL_BUTTONS } from "../../data/constants.js"
+
+export const createModalButtons = (parentElement) => {
+
+    for (const key in MODAL_BUTTONS) {
+        const buttonEl = document.createElement('button')
+        buttonEl.innerHTML = MODAL_BUTTONS[key]
+        buttonEl.id = key
+        parentElement.appendChild(buttonEl)
+    }
+}
