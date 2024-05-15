e3ebf951dcf15ef938c5ea275cd0c0839fb1602c
jgchoti <jgchotirat@gmail.com>
2024-05-15 20:43:06 +0200

    Update init.js

--- client/src/init.js
+++ client/src/init.js
@@ -1,6 +1,5 @@
 import { tableHeader } from './components/table-header.js'
 import { header } from './components/header.js'
-import { createTable } from './components/table.js'
 import { newListButton } from './components/new-list-button.js'
 import { newListListeners } from './listeners/new-list-listeners.js'
 import { saveList } from './handlers/save-list.js'
