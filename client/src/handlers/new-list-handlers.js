ba60d931f4cc0a34262429905c9b63de2f06890e
jgchoti <jgchotirat@gmail.com>
2024-05-15 20:43:04 +0200

    Update new-list-handlers.js

--- client/src/handlers/new-list-handlers.js
+++ client/src/handlers/new-list-handlers.js
@@ -1,6 +1,6 @@
-import { createTable } from '../components/table.js'
 import { NEW_TASK } from '../../data/constants.js'
+import { loadTable } from '../components/render-table.js'
 
 export const newListHandlers = () => {
-    createTable(NEW_TASK)
+    loadTable(NEW_TASK)
 }
