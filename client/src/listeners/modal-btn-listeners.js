6ef678bcbff3e0635ad3e30bf99a00b92fdeb770
jgchoti <jgchotirat@gmail.com>
2024-05-16 14:35:27 +0200

    Update modal-btn-listeners.js

--- client/src/listeners/modal-btn-listeners.js
+++ client/src/listeners/modal-btn-listeners.js
@@ -2,10 +2,12 @@ import { CLICK, MODAL_BUTTONS_HANDLERS } from '../../data/constants.js'
 
 
 export const modalBtnListeners = () => {
+
     for (const key in MODAL_BUTTONS_HANDLERS) {
         const buttonEl = document.getElementById(key);
         if (buttonEl) {
             buttonEl.addEventListener(CLICK, MODAL_BUTTONS_HANDLERS[key]);
         }
     }
+
 };
