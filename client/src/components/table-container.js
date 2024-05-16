d531938015d45ab70954552c07476ffa6b24b1b9
jgchoti <jgchotirat@gmail.com>
2024-05-16 16:32:07 +0200

    Create table-container.js

---
+++ client/src/components/table-container.js
@@ -0,0 +1,15 @@
+import { TABLE_CONTAINER_ID } from "../../data/constants.js"
+import { header } from './header.js'
+import { tableHeader } from './table-header.js'
+
+export const tableContainer = () => {
+    const rootContainer = document.getElementById('root')
+    const divEl = document.createElement('div')
+    divEl.id = TABLE_CONTAINER_ID
+    rootContainer.appendChild(divEl)
+
+    header()
+    tableHeader()
+
+    return divEl
+}
