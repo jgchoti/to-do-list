b74858ac5c35f1ed3cd8c22c92497ea1f383e95b
jgchoti <jgchotirat@gmail.com>
2024-05-16 16:31:49 +0200

    Update init.js

--- client/src/init.js
+++ client/src/init.js
@@ -1,30 +1,13 @@
-import { tableHeader } from './components/table-header.js'
-import { header } from './components/header.js'
-import { newListButton } from './components/new-list-button.js'
-import { newListListeners } from './listeners/new-list-listeners.js'
-import { saveList } from './handlers/save-list.js'
-import { state } from '../data/state.js'
+import { tableContainer } from './components/table-container.js'
 import { loadList } from './handlers/load-list.js'
-import { saveButton } from './components/save-button.js'
-import { saveButtonListeners } from './listeners/save-listeners.js'
-import { resetButton } from './components/reset-button.js'
-import { resetListeners } from './listeners/reset-listeners.js'
 import { createButtons } from './components/buttons.js'
 import { buttonsListeners } from './listeners/buttons-listeners.js'
 import { modal } from './components/modal.js'
-import { modalBtnListeners } from './listeners/modal-btn-listeners.js'
 
-tableHeader()
-header()
+
+tableContainer()
 loadList()
 createButtons()
-buttonsListeners()
 modal()
-modalBtnListeners()
+buttonsListeners()
 
-// newListButton()
-// saveButton()
-// resetButton()
-// newListListeners()
-// saveButtonListeners()
-// resetListeners()
