b64afee349a46c58a036e81d83b4b3981a02e143
jgchoti <jgchotirat@gmail.com>
2024-05-17 15:10:40 +0200

    Create render-buttons.js

---
+++ client/src/components/render-buttons.js
@@ -0,0 +1,12 @@
+import { BUTTONS } from "../../data/constants.js"
+
+export const renderButtons = () => {
+    const container = document.getElementById('table-container')
+    for (const key in BUTTONS) {
+        const buttonEl = document.createElement('button')
+        buttonEl.innerHTML = BUTTONS[key]
+        buttonEl.id = key
+        container.appendChild(buttonEl)
+
+    }
+}
