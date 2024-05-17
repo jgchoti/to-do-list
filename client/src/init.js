1489fb9de5f101b88384da883190f3ff90b13742
jgchoti <jgchotirat@gmail.com>
2024-05-17 15:10:06 +0200

    Update init.js

--- client/src/init.js
+++ client/src/init.js
@@ -1,13 +1,13 @@
 import { tableContainer } from './components/table-container.js'
 import { loadList } from './handlers/load-list.js'
-import { createButtons } from './components/buttons.js'
+import { renderButtons } from './components/render-buttons.js'
 import { buttonsListeners } from './listeners/buttons-listeners.js'
-import { modal } from './components/modal.js'
+import { taskEntryModal } from './components/task-entry-modal.js'
 
 
 tableContainer()
 loadList()
-createButtons()
-modal()
+renderButtons()
+taskEntryModal()
 buttonsListeners()
 
