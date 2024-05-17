077e963c74a7360ad6296af5af62d81b01149d5a
jgchoti <jgchotirat@gmail.com>
2024-05-17 15:39:06 +0200

    format

--- client/src/components/text-input.js
+++ client/src/components/text-input.js
@@ -1,9 +1,9 @@
 export const textInput = (text) => {
-  const labelEl = document.createElement('label');
-  const pEl = document.createElement('p');
-  pEl.innerText = text;
-  pEl.setAttribute('contenteditable', 'true');
-  // labelEl.setAttribute('for', taskId);
-  labelEl.appendChild(pEl);
-  return labelEl;
+	const labelEl = document.createElement('label');
+	const pEl = document.createElement('p');
+	pEl.innerText = text;
+	pEl.setAttribute('contenteditable', 'true');
+	// labelEl.setAttribute('for', taskId);
+	labelEl.appendChild(pEl);
+	return labelEl;
 };
