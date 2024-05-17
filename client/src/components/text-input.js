bc3f9a8cb5fc5c8b36c948d413f1c524c757b6e0
jgchoti <jgchotirat@gmail.com>
2024-05-17 15:29:00 +0200

    Update text-input.js

--- client/src/components/text-input.js
+++ client/src/components/text-input.js
@@ -1,9 +1,9 @@
-export const textInput = (text, taskId, attachListener = true) => {
-	const labelEl = document.createElement('label');
-	const pEl = document.createElement('p');
-	pEl.innerText = text;
-	pEl.setAttribute('contenteditable', 'true');
-	// labelEl.setAttribute('for', taskId);
-	labelEl.appendChild(pEl);
-	return labelEl;
+export const textInput = (text) => {
+  const labelEl = document.createElement('label');
+  const pEl = document.createElement('p');
+  pEl.innerText = text;
+  pEl.setAttribute('contenteditable', 'true');
+  // labelEl.setAttribute('for', taskId);
+  labelEl.appendChild(pEl);
+  return labelEl;
 };
