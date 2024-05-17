fdc01d15145c1b1f7ca42ba6b8628d1e7f0a9822
jgchoti <jgchotirat@gmail.com>
2024-05-17 15:09:50 +0200

    Delete reset-listeners.js

--- client/src/listeners/reset-listeners.js
+++
@@ -1,7 +0,0 @@
-import { RESET_BTN_ID, CLICK } from '../../data/constants.js'
-import { reset } from '../handlers/reset.js'
-
-export const resetListeners = () => {
-    const button = document.getElementById(RESET_BTN_ID)
-    button.addEventListener(CLICK, reset)
-}
